a5148bd6f8af0933414aad2470d9d7c3c070ae08
Tobi Olanitori <tobiolanitori@gmail.com>
2023-04-13 07:40:32 +0100

    feat: implemented DB transactions on the user register route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use App\Models\Guardian;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
@@ -17,52 +18,58 @@ class AuthController extends Controller
      */
     public function register(StoreUserRequest $request)
     {
-        $user_type = $request->user_type;
-        // Create a new User instance and set the attributes
-        $user = new User();
-        $user->first_name = $request->first_name;
-        $user->last_name = $request->last_name;
-        $user->email = $request->email;
-        $user->school_id = $request->school_id;
-        $user->user_type = $request->user_type;
-        $user->setBcryptPasswordAttribute($request->password); // hash the password
-        $user->save();
+        $guardianCode = DB::transaction(function () use ($request) {
+            $user_type = $request->user_type;
+            // Create a new User instance and set the attributes
+            $user = new User();
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->email = $request->email;
+            $user->school_id = $request->school_id;
+            $user->user_type = $request->user_type;
+            $user->setBcryptPasswordAttribute($request->password); // hash the password
+            $user->save();
 
-        if ($user_type === 'guardian') {
-            // Generate a unique guardian code
-            $guardianCode = mt_rand(100000, 999999);
-            while (Guardian::where('guardian_code', $guardianCode)->exists()) {
+            // When user is a guardian
+            if ($user_type === 'guardian') {
+                // Generate a unique guardian code
                 $guardianCode = mt_rand(100000, 999999);
+                while (Guardian::where('guardian_code', $guardianCode)->exists()) {
+                    $guardianCode = mt_rand(100000, 999999);
+                }
+
+                // Create a new guardian record
+                $guardian = new Guardian([
+                    'guardian_code' => $guardianCode,
+                    'user_id' => $user->id,
+                ]);
+                $user->guardian()->associate($guardian);
+                $user->save();
             }
 
-            // Create a new guardian record
-            $guardian = new Guardian([
-                'guardian_code' => $guardianCode,
-                'user_id' => $user->id,
-            ]);
-            $user->guardian()->save($guardian);
-        }
-
-        if ($user_type === 'student') {
-            $guardianCode = $request->guardian_code;
-            if (!$guardianCode) throw new CreateApiException('Provide guardian code', 422);
-            $student = new Student([
-                'guardian_code' => $guardianCode,
-                'user_id' => $user->id,
-            ]);
-            $user->student()->save($student);
-        }
+            // Whwn user is a student
+            if ($user_type === 'student') {
+                $guardianCode = $request->guardian_code;
+                if (!$guardianCode) throw new CreateApiException('Provide guardian code', 422);
+                $student = new Student([
+                    'guardian_code' => $guardianCode,
+                    'user_id' => $user->id,
+                ]);
+                $user->student()->save($student);
+            }
+            return $guardianCode;
+        });
 
 
 
         // Return a success response
         return new JsonResponse([
             'success' => true,
-            'message' => 'User registered successfully',
             'data' => [
                 'name' => $request->first_name . ' ' . $request->last_name,
                 'guardian_code' => $guardianCode
-            ]
+            ],
+            'message' => 'success',
         ]);
     }
 

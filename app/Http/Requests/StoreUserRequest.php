d50ffaf488062b755911e41f31dee3fa6a611cb5
Tobi Olanitori <tobiolanitori@gmail.com>
2023-05-06 06:48:53 +0100

    chore: more specific validation for user registration

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -27,10 +27,23 @@ class StoreUserRequest extends FormRequest
         return [
             'email' => 'required|email|unique:users,email',
             'password' => 'required',
-            'school_id' => 'required',
+            'school_id' => 'required|integer',
             'user_type' => ['required', Rule::in(['guardian', 'student'])],
-            'first_name' => 'required',
-            'last_name' => 'required'
+            'first_name' => ['required'],
+            'last_name' => ['required']
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'school_id.required' => 'school Id is a required field',
+            'school_id.integer' => 'school Id must be an Integer',
         ];
     }
 
@@ -42,8 +55,15 @@ class StoreUserRequest extends FormRequest
      */
     protected function failedValidation($validator)
     {
-        // var_dump('here 2');
         // If email is not unique throw 409
+        if (isset($validator->failed()['email']['Unique'])) {
+            throw new CreateApiException($validator->errors()->first(), 409);
+        }
+
+        // If school ID is not an integer, throw a 400
+        if (isset($validator->failed()['school_id']['Integer'])) {
+            throw new CreateApiException($validator->errors()->first(), 400);
+        }
 
         // else throw 422
         throw new CreateApiException($validator->errors()->first(), 422);
